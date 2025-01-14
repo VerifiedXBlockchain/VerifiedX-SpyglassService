b08cd326091539777e6e66a57ac9595303ec2276
Tyler Savery <tyler@theyoungastronauts.com>
2025-01-14 15:10:26 -0500

    trigger command low level

--- project/celery.py
+++ project/celery.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 from celery import Celery
@@ -16,7 +17,7 @@ app.autodiscover_tasks()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
 
-    sender.add_periodic_task(3 * 60, sync_master_nodes.s(), name="Sync Master Nodes")
+    sender.add_periodic_task(10 * 60, sync_master_nodes.s(), name="Sync Master Nodes")
     sender.add_periodic_task(10, sync_blocks.s(), name="Sync Blocks")
 
     sender.add_periodic_task(5 * 60, update_cmc_prices.s(), name="Update CMC Prices")
@@ -41,9 +42,19 @@ def setup_periodic_tasks(sender, **kwargs):
 
 @app.task
 def sync_blocks():
-    from django.core import management
+    # from django.core import management
 
-    management.call_command("sync_blocks")
+    # management.call_command("sync_blocks")
+
+    print("TRIGGERING sync_blocks() command in celery.py")
+    command = ["python", "manage.py", "sync_blocks"]
+
+    # Run the command in your home directory
+    result = subprocess.run(command, cwd="~", capture_output=True, text=True)
+
+    # Print the output
+    print(result.stdout)
+    print("-------")
 
 
 @app.task
