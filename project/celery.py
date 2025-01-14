71f7552f3d19e76caa951763b7c5c5cec6bd20f5
Tyler Savery <tyler@theyoungastronauts.com>
2025-01-14 15:19:37 -0500

    changed name of the task command

--- project/celery.py
+++ project/celery.py
@@ -18,7 +18,7 @@ app.autodiscover_tasks()
 def setup_periodic_tasks(sender, **kwargs):
 
     sender.add_periodic_task(10 * 60, sync_master_nodes.s(), name="Sync Master Nodes")
-    sender.add_periodic_task(10, sync_blocks.s(), name="Sync Blocks")
+    sender.add_periodic_task(10, sync_the_blocks.s(), name="Sync Blocks")
 
     sender.add_periodic_task(5 * 60, update_cmc_prices.s(), name="Update CMC Prices")
 
@@ -41,7 +41,7 @@ def setup_periodic_tasks(sender, **kwargs):
 
 
 @app.task
-def sync_blocks():
+def sync_the_blocks():
     # from django.core import management
 
     # management.call_command("sync_blocks")
