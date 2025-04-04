dbf80426d35bf8b672669eb20ccbdcca11876307
Tyler Savery <tyler@theyoungastronauts.com>
2025-04-04 11:13:43 -0400

    request.data

--- api/galxe/views.py
+++ api/galxe/views.py
@@ -6,9 +6,15 @@ class GalxeVerifyView(GenericAPIView):
 
     def post(self, request, *args, **kwargs):
 
-        address = request.POST.get("address", None)
+        address = request.data["address"]
 
         if not address:
             return Response({"success": False, "message": "Address required"})
+
+        # Lookup btc address on mempool
+
+        # Look for outgoing txs
+
+        # find one with a vBTC deposit address
 
         return Response({"success": True, "message": f"Address: {address}"})
