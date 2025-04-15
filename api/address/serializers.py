3ced568be58d4796a018f280002a20552ff42baa
Tyler Savery <tyler@theyoungastronauts.com>
2025-04-15 16:18:33 -0400

    serialize domain

--- api/address/serializers.py
+++ api/address/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+from api.adnr.serializers import AdnrSerializer
 from rbx.models import Address
 
 
 class AddressSerializer(serializers.ModelSerializer):
+    adnr = AdnrSerializer(required=False)
+
     class Meta:
         model = Address
         fields = [
